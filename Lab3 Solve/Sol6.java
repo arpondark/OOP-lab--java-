edf55f669f4f0fbd9755802232232ab88a6e7c31
MD SHAZAN MAHMUD ARPON <arponarpon007@gmail.com>
2024-11-29 13:24:58 +0600

    final

--- Lab3 Solve/Sol6.java
+++ Lab3 Solve/Sol6.java
@@ -4,7 +4,7 @@ public class Sol6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[10];
-        boolean[] isPresent = new boolean[100]; // Assuming the range of input numbers is 0 to 99
+        boolean[] isPresent = new boolean[100];
 
         System.out.println("Enter ten numbers:");
 
@@ -25,13 +25,12 @@ public class Sol6 {
         // Display distinct numbers
         System.out.print("Distinct numbers: ");
         for (int i = 0; i < 10; i++) {
-            if (numbers[i] != 0) { // Assuming 0 is not used as an input number
+            if (numbers[i] != 0) {
                 System.out.print(numbers[i] + " ");
             }
         }
     }
 
-    // Helper method to check if a number is already present in the array
     public static boolean isDistinct(int[] numbers, boolean[] isPresent, int num) {
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == num) {
@@ -41,11 +40,10 @@ public class Sol6 {
         return false;
     }
 
-    // Helper method to count distinct numbers
     public static int countDistinctNumbers(int[] numbers) {
         int count = 0;
         for (int num : numbers) {
-            if (num != 0) { // Assuming 0 is not used as an input number
+            if (num != 0) {
                 count++;
             }
         }
